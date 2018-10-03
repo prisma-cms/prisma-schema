b9cfa2054d8572e53048581d9352cb5dbb168c06
Fi1osof <n.lanets@modxclub.ru>
2018-10-03 17:48:12 +0300

    1.0.2-beta

--- src/index.js
+++ src/index.js
@@ -1,1 +1,51 @@
-export default 'Welcome to @prisma-cms/prisma-schema'
+ 
+
+import fs from "fs";
+
+const { parse, print } = require('graphql');
+
+// const core = new CoreModule();
+
+
+const generateSchema = function (schemaType, coreModule, basePath) {
+
+  let schema;
+
+
+  switch (schemaType) {
+
+    case "prisma":
+
+      schema = coreModule.getSchema();
+ 
+
+      const parsed = parse(schema);
+
+      parsed.definitions = parsed.definitions.filter(
+        n => n.kind !== "SchemaDefinition" && !(
+          n.kind === "ObjectTypeDefinition" && ["Query", "Mutation", "Subscription"].indexOf(n.name.value) !== -1)
+      );
+
+      schema = print(parsed);
+
+      fs.writeFileSync(basePath + "/prisma/generated.graphql", schema);
+
+      break;
+
+
+    case "api":
+
+      schema = coreModule.getApiSchema();
+
+      fs.writeFileSync(basePath + "/generated/api.graphql", schema);
+
+
+      break;
+
+    default: throw new Error("Env schemaType is wrong or not defined");
+
+  }
+
+}
+
+export default generateSchema;
